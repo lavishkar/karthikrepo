4499adab2800d8ec9c817885fb6c0e8785b78efe
lavanyakarthik.cbe@gmail.com <Karthik@192.168.0.4>
2023-03-24 21:49:48 -0400

    Hi

---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,15 @@
+package Calculator;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 100;
+		int b= 100;
+		int c= a+ b;
+		System.out.println(c);
+		
+
+	}
+
+}
